ea4a195b4b0fc14c65d647208e584aa531c5ef08
nmecham27 <nmecham27@gmail.com>
2023-11-04 17:58:13 -0600

    Added simple 8 bit encryption block. (#11)

---
+++ encryption-block/one_time_pad_encryption_tb.v
@@ -0,0 +1,112 @@
+`timescale 1ns/1ps
+module otp_encryption_decryption_tb;
+  reg [7:0] input_data;
+  wire [7:0] output_data;
+  reg rst;
+  reg passthrough;
+  reg start;
+  wire done;
+
+  otp_encryption_decryption dut (
+    .input_data(input_data),
+    .reset(rst),
+    .passthrough(passthrough),
+    .start(start),
+    .output_data(output_data),
+    .done(done)
+  );
+
+  reg [7:0] output_holder;
+
+  initial begin
+    rst = 1;
+    #50;
+    rst = 0;
+    #10;
+    
+    start = 1'b0;
+    passthrough = 1'b0;
+    input_data = 8'h33;
+    start = 1'b1;
+    #5; // We need to hold start high for a small amount of time
+    start = 1'b0;
+
+    while(!done) begin
+      #100;
+    end
+
+    //output_holder = output_data;
+
+    $display("Data to encrypt: %h", input_data);
+    $display("Encrypted Data: %h", output_data);
+
+    #10;
+
+    input_data = output_data;
+    start = 1'b1;
+    #5; // We need to hold start high for a small amount of time
+    start = 1'b0;
+    
+    while(!done) begin
+      #1;
+    end
+
+    $display("Decrypted Data: %h", output_data);
+
+    #50;
+
+    input_data = 8'h27;
+    start = 1'b1;
+    #5; // We need to hold start high for a small amount of time
+    start = 1'b0;
+    
+    while(!done) begin
+      #1;
+    end
+
+    $display("Data to encrypt: %h", input_data);
+    $display("Encrypted Data: %h", output_data);
+
+    #10;
+
+    input_data = output_data;
+    start = 1'b1;
+    #5; // We need to hold start high for a small amount of time
+    start = 1'b0;
+    
+    while(!done) begin
+      #1;
+    end
+
+    $display("Decrypted Data: %h", output_data);
+
+    #10;
+
+    // Test a plain text value
+    passthrough = 1'b1;
+    input_data = 8'hDE;
+    start = 1'b1;
+    #5; // We need to hold start high for a small amount of time
+    start = 1'b0;
+    
+    while(!done) begin
+      #1;
+    end
+
+    $display("Data to encrypt: %h", input_data);
+    $display("Encrypted Data: %h", output_data);
+
+    #10;
+
+    input_data = output_data;
+    start = 1'b1;
+    #5; // We need to hold start high for a small amount of time
+    start = 1'b0;
+    
+    while(!done) begin
+      #1;
+    end
+
+    $display("Decrypted Data: %h", output_data);
+  end
+endmodule
